953282e144e86299b20b1c73ad270093b81d7217
Kian Walsh <64419198+KianWalsh@users.noreply.github.com>
2023-07-26 19:24:54 -0400

    ReImplemented Code
    rewrote code to be a cpp library and a wrapper, so that external functions call the program the same and cpp programs can compile with this library

--- README.md
+++ README.md
@@ -1,24 +1,8 @@
-cppScrape, a simple curl program to extract specified html/xml tags from a given link.
 
-By Kian Walsh 
+***INTRODUCTION***
+cppScrape is a simple cpp library + wrapper ment to be able to scrape webpages and sort by html tags. 
+By Kian Walsh
+kianlwalsh@gmail.com
 
-***CONFIGURE***
-So far, there is 2 major configuration options I have implemented, the taglist[] in the cppScrape.cpp file containes a array 
-of html <tags>. to change how the output.tsv file formats tag content. to change, add or remove tags, changing indexes will
-change what row the tag results will be placed, adding extra tags will command the program to check for those as well.
+***LIBRARY USAGE***
 
-IE. taglist[] of "[h1,p"]" will return {all text marked with h1},{all text marked with p}
-
-the second config is a work in progress, if the program is called by "cppScrape https://exampe.com u", the program will 
-use the url given as the file name (https://example.com.tsv)
-
-***COMPILE***
-I have build this programs using msvc and g++, with the only non-standard library being libcurl (the c version, not the c++).
-With your compiler and code installed, a simple command "g++ cppScrape.cpp -lcurl" command will build the program the executable.
-
-***USAGE***
-I wrote this to be as simple as possible for anyone else (or any other programs) to use, all you have to do is call "cppScrape <url>" 
-with a valid url, and the program will output a .tsv file, with each row containing all text written with the tag specified in taglist[]
-by default, the program will find and record the title, h1-h6 tag content, and p tag content in a .tsv file
-
-<b>open a issue</b>, or email me at kianlwalsh@gmail.com for questions/suggestions 
---
+++ cppScrape.hpp
@@ -0,0 +1,145 @@
+#include <curl/curl.h>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+class Scrape {
+private:
+	std::string returnedHTML = "";//actual recorded responce by external functions 
+public:
+
+	//Configuration FUnctions 
+	std::string URL = "http://example.com";//The target html pages address.
+	std::string FILENAME = "out.tsv";//filename of new file created by parseByTags().
+	std::vector<std::string> TAGS;//A list of tags to look for on the target webpage 
+	void setURL(std::string link) {//Set a target address to read from.
+		URL = link;
+	}
+	void setFileName(std::string name) {//set the filename of the new file created by parseByTags()
+		FILENAME = name;
+	}
+	void addTag(std::string tag) {//Add tags to be looked for by parseByTags. at least one tag is needed to call parseByTags. will be added to the lowest row of output file 
+		TAGS.push_back(tag);
+	}
+	void addTag(std::string tag, int index) {//Add tags to be looked for by parseByTags. at least one tag is needed to call parseByTags. will be added to the lowest row of output file 
+		TAGS.insert(TAGS.begin() + index + index, tag);
+	}
+
+	//Helper functions for CURL
+	static size_t writeRes(char* contents, size_t size, size_t nmemb, void* userp)//helper function to write the data from GET to a string 
+	{
+		((std::string*)userp)->append((char*)contents, size * nmemb);
+		return size * nmemb;
+	}
+	std::string makeEndTag(std::string tag) {//quick function that converts <tags> to closed </tags>
+		return{ "</" + tag.substr(1) };
+	}
+
+	//core functons 
+	std::string sentRequest() {//GET the address at URL, allowing for parseByTags() to run. returns A COPY of the stream of data returned from GET
+		CURL* curl;
+		CURLcode output;
+
+		curl = curl_easy_init();
+		if (curl) {
+			curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Scrape::writeRes);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &returnedHTML);
+			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+			output = curl_easy_perform(curl);
+			if (output != CURLE_OK) {
+				fprintf(stderr, "E: curl_easy_preform() failed: %s\n", curl_easy_strerror(output));
+			}
+			curl_easy_cleanup(curl);
+		}
+		else {
+			std::cout << "E: curl initialization error\n";
+		}
+		return(returnedHTML);
+	}
+	auto parseByTags() {//Find each tag in TAGS, and writes every instance in order of occurence in filename.tsv
+		if (TAGS.size() == 0) {
+			std::cout << "CONFIGURATION ERROR, no tags given, aborting";
+			return;
+		}
+		if (returnedHTML == "") {
+			std::cout << "CONFIGURATION ERROR, you must sendrequest() before you can call parseByTag(), aborting";
+			return;
+		}
+		//all lowercase
+		for (char& c : returnedHTML) {
+			c = tolower(c);
+		}
+		while (returnedHTML.find("\n") != std::string::npos) {
+			int index = returnedHTML.find("\n");
+			int length = 1;
+			returnedHTML.erase(index, length);
+		}
+
+
+		//remove all tabs (tabs denote next column in .tsv formats)
+		while (returnedHTML.find("\t") != std::string::npos) {
+			int index = returnedHTML.find("\t");
+			int length = 1;
+			returnedHTML.erase(index, length);
+		}
+
+
+		//removing style and script to make prossessing easier 
+		while (returnedHTML.find("<script") != std::string::npos) {
+			int index = returnedHTML.find("<script");
+			int length = returnedHTML.find("</script>") - index + 9;
+			returnedHTML.erase(index, length);
+		}
+		while (returnedHTML.find("<style") != std::string::npos) {
+			int index = returnedHTML.find("<style");
+			int length = returnedHTML.find("</style>") - index + 8;
+			returnedHTML.erase(index, length);
+		}
+
+		//remove yahoo finances horrible quote markers
+		while (returnedHTML.find("&quot;") != std::string::npos) {
+			int index = returnedHTML.find("&quot;");
+			int length = 6;
+			returnedHTML.erase(index, length);
+		}
+
+		std::ofstream out(FILENAME.c_str());//output file 
+
+		std::vector<std::string> toWrite(TAGS.size(), "");//containes list of strings, each corresponding to a html tag
+
+		for (int i = 0; i < static_cast<int>(TAGS.size()); i++) {
+			std::string tag = TAGS.at(i);
+			std::string endTag = makeEndTag(tag);
+			while (returnedHTML.find(tag) != std::string::npos) {
+				int currentIndex = returnedHTML.find(tag);
+				int nextIndex = returnedHTML.find(endTag);
+				if (currentIndex < nextIndex) {
+					toWrite.at(i) += returnedHTML.substr(currentIndex, nextIndex - currentIndex + endTag.length()) + "\t";
+					returnedHTML.erase(currentIndex, nextIndex - currentIndex + endTag.length());
+				}
+				else {
+					break;
+				}
+			}
+			//remove any extra long spaces
+			while (toWrite.at(i).find("  ") != std::string::npos) {
+				int index = toWrite.at(i).find("  ");
+				toWrite.at(i).erase(index, 1);
+			}
+			//remove and nested tags within target tags
+			while (toWrite.at(i).find("<") != std::string::npos) {
+				int index = toWrite.at(i).find("<");
+				int length = toWrite.at(i).find(">") - index + 1;
+				toWrite.at(i).erase(index, length);
+			}
+		}
+		//write queue to file 
+		for (int i = 0; i < static_cast<int>(toWrite.size()); i++) {
+			out << toWrite[i] << "\n";
+		}
+		out.close();
+	}
+
+
+};
